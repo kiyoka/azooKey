cb76014e44420605250df06aed9aac3ba89aacc4
Kiyoka Nishiyama <kiyokasumibi@gmail.com>
2025-12-07 20:24:07 +0900

    Xcode 26.1.1で重複エラーとなる問題を修正

--- AzooKeyCore/Package.swift
+++ AzooKeyCore/Package.swift
@@ -8,28 +8,8 @@ let swiftSettings: [SwiftSetting] = [
     .interoperabilityMode(.Cxx),
 ]
 
-#if canImport(FoundationModels)
-let isXcodeVersion26 = true
-#else
-let isXcodeVersion26 = false
-#endif
-
-let xcode26AdditionalTargets: [Target] = [
-    .binaryTarget(
-        // Note: Xcode 26以降、AzooKeyKanaKanjiConverter側のXCFrameworkのbinaryTargetをXcodeが解決してくれなくなった。
-        // そこで、binaryTargetを再度AzooKeyCore側でも要求することで、結果的に認識されるようになる。
-        // さらに`AzooKeyUtils`でも`llama`を要求しないとビルドは通らない。
-        // ただし、Xcode 26より前の場合は逆にこの対応を入れると動作しないので、Xcodeバージョンを確認する必要がある
-        name: "llama",
-        url: "https://github.com/azooKey/llama.cpp/releases/download/b4846/signed-llama.xcframework.zip",
-        // this can be computed `swift package compute-checksum llama-b4844-xcframework.zip`
-        checksum: "db3b13169df8870375f212e6ac21194225f1c85f7911d595ab64c8c790068e0a"
-    ),
-]
-
-let xcode26AdditionalTargetDependency: [Target.Dependency] = [
-    "llama"
-]
+// Note: Xcode 26.1以降では、AzooKeyKanaKanjiConverter側のllama.cpp binaryTargetが正しく解決されるため、
+// 追加のbinaryTargetは不要になりました。以前のXcode 26.0では必要でしたが、26.1.1では重複エラーの原因となります。
 
 let package = Package(
     name: "AzooKeyCore",
@@ -103,7 +83,7 @@ let package = Package(
                 "KeyboardThemes",
                 "KeyboardViews",
                 .product(name: "OpenAI", package: "OpenAI"),
-            ] + (isXcodeVersion26 ? xcode26AdditionalTargetDependency : []),
+            ],
             resources: [],
             swiftSettings: swiftSettings
         ),
@@ -130,5 +110,5 @@ let package = Package(
             ],
             swiftSettings: swiftSettings
         ),
-    ] + (isXcodeVersion26 ? xcode26AdditionalTargets : [])
+    ]
 )
